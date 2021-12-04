c034b0df679ff90a86437698961a31ea684a580c
Alexnorvag <norvag@mail.ru>
2021-12-04 15:18:28 +0300

    added: reusable text field component

--- src/pages/KPIs/components/KPIsForm/KPIsForm.js
+++ src/pages/KPIs/components/KPIsForm/KPIsForm.js
@@ -1,12 +1,62 @@
+import { useState } from "react";
+
 import Select from "../../../../components/common/Select/Select";
 import Chip from "../../../../components/common/Chip/Chip";
 import { useKPIsData } from "../../state/hooks/useKPIsData";
+import TextField from "../../../../components/common/Inputs/TextField";
 
 const KPIsForm = () => {
   const { kpis, selectedKPIs, kpisTags, updateSelectedKPI } = useKPIsData();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+
+  const toggleIsPasswordShown = () => {
+    console.log("onToggle: ");
+    setIsPasswordShown((isShown) => !isShown);
+  };
+
+  const inputs = [
+    {
+      id: "name",
+      value: name,
+      onChange: setName,
+      label: "Full Name",
+    },
+    {
+      id: "email",
+      value: email,
+      onChange: setEmail,
+      label: "Email",
+      placeholder: "Llalalal",
+    },
+    {
+      id: "phone",
+      value: phone,
+      onChange: setPhone,
+      label: "Phone No",
+      size: "small",
+    },
+    {
+      id: "password",
+      value: password,
+      onChange: setPassword,
+      label: "Password",
+      size: "small",
+      isToggled: isPasswordShown,
+      onToggle: toggleIsPasswordShown,
+      type: isPasswordShown ? "text" : "password",
+    },
+  ];
 
   return (
     <div className="kpis-form">
+      {inputs.map((input) => (
+        <TextField key={input.id} {...input} />
+      ))}
+
       {selectedKPIs.map((selectedKPI, kpiIdx) => (
         <div className={`kpi-${kpiIdx}`} key={kpiIdx}>
           <Select

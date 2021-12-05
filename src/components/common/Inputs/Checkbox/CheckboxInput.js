421f85be6a8755728f8c9ae313faedca715781aa
Alexnorvag <norvag@mail.ru>
2021-12-05 07:39:58 +0300

    added: selectable list item component

---
+++ src/components/common/Inputs/Checkbox/CheckboxInput.js
@@ -0,0 +1,13 @@
+import './CheckboxInput.scss';
+
+const CheckboxInput = ({ id, checked, onChange }) => (
+  <input
+    id={id}
+    type="checkbox"
+    className="custom-checkbox uk-checkbox"
+    checked={checked}
+    onChange={onChange}
+  />
+);
+
+export default CheckboxInput;
